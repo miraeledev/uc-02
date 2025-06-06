bae07bd4640e7457f63f1336599eba1a0dab2657
miraeledev <miraele14@gmail.com>
2025-06-06 16:39:49 -0300

    revisao

---
+++ Aula12/revisao.js
@@ -0,0 +1,32 @@
+// for (let i = 0; i <= 10; i++) {
+    // console.log(`Número: ${i}`);   
+// }
+
+// let i = 0; 
+// while (i <= 10) {
+//     console.log(`número: ${i}`);
+//     i++
+// }
+
+// let i = 0
+// do {
+//     console.log(`Número: ${i}`);
+//     i++;
+// } while (i <= 10);
+
+// Use um laço `for` para exibir a tabuada do número 5 (de 5 x 1 até 5 x 10).
+
+// const prompt = require('prompt-sync')();
+// const numero = parseInt(prompt("Digite um número de 0 a 10: "));
+// for (let i = 0; i <= 10; i++){
+
+//     console.log(`${numero} x ${i} = ` + numero * i )
+// }
+
+// Use um acumulador dentro de um laço para somar todos os números de 1 até 100.
+
+const prompt = require('prompt-sync')();
+const numero = parseInt(prompt("Digite um número de 1 a 100: "));
+for (let i = 1; i <= 100; i++)
+    {
+        console.log()
